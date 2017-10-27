1005a00a9cbc4c9a48e93624eda0046be2fab1b5
hqxh <2799192915@qq.com>
2017-10-27 15:43:51 +0800

    加入显示信息功能

--- app/src/main/java/com/example/administrator/coolweather/util/Utility.java
+++ app/src/main/java/com/example/administrator/coolweather/util/Utility.java
@@ -6,6 +6,8 @@ import android.util.Log;
 import com.example.administrator.coolweather.db.City;
 import com.example.administrator.coolweather.db.County;
 import com.example.administrator.coolweather.db.Province;
+import com.example.administrator.coolweather.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -16,6 +18,18 @@ import org.json.JSONObject;
  */
 
 public class Utility {
+    public static Weather handleWeatherResponse(String response){
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray =jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
     public static boolean handleProvinceResponse(String response){
         if(!TextUtils.isEmpty(response)){
             try {
